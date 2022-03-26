a194fcb651711e4805af51866dc387a305d4bbf6
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-03-26 20:56:29 -0300

    Create balance.js

---
+++ commands/command/balance.js
@@ -0,0 +1,15 @@
+module.exports ={
+  name: "balance",
+  aliases:["b","bal","money"],
+  code:`$addField[1;\`🌰\` Chestnuts;\`\`\`$getGlobalUserVar[CHESTNUT;$mentioned[1]]\`\`\`]
+$addField[1;\`💎\` Gems;\`\`\`$getGlobalUserVar[GEMS;$mentioned[1]]\`\`\`]
+$wait[1s]
+$title[1;$username[$mentioned[1]]'s balance]
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]
+
+$reply[$messageID;yes]`
+}
