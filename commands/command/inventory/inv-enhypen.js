7a2f480c0bbe19da47a4dfa920856f162cb68fe1
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-03-26 21:00:05 -0300

    Add files via upload

---
+++ commands/command/inventory/inv-enhypen.js
@@ -0,0 +1,45 @@
+module.exports = {
+  name: "inv enhypen",
+  $if: "v4",
+  code:`
+
+$author[1;Inventory — ENHYPEN;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$title[1;\`$getGlobalUserVar[ENHS_2_DA_RARITY]\` $getGlobalUserVar[ENHS_2_DA_THEME]]
+
+$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
+
+$if[$getGlobalUserVar[ENHS_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENHS_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENHS_2_DA]\`
+Code: \`ENHS_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENJY_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENJY_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENJY_2_DA]\`
+Code: \`ENJY_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENJK_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENJK_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENJK_2_DA]\`
+Code: \`ENJK_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENSH_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENSH_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENSH_2_DA]\`
+Code: \`ENSH_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENSN_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENSN_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENSN_2_DA]\`
+Code: \`ENSN_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENJW_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENJW_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENJW_2_DA]\`
+Code: \`ENJW_2_DA\`;yes]
+$endif
+$if[$getGlobalUserVar[ENNK_2_DA]>=1]
+$addField[1;<:JJU_enhypen:957393538260271154> $getGlobalUserVar[ENNK_2_DA_IDOL];Copies: \`$getGlobalUserVar[ENNK_2_DA]\`
+Code: \`ENNK_2_DA\`;yes]
+$endif
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
---
+++ commands/command/inventory/inv-redvelvet.js
@@ -0,0 +1,37 @@
+module.exports = {
+  name: "inv redvelvet",
+  $if: "v4",
+  code:`
+
+$author[1;Inventory — Red Velvet;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$title[1;\`$getGlobalUserVar[RVIR_2_R2_RARITY]\` $getGlobalUserVar[RVIR_2_R2_THEME]]
+
+$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
+
+$if[$getGlobalUserVar[RVYR_2_R2]>=1]
+$addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVYR_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVYR_2_R2]\`
+Code: \`RVYR_2_R2\`;yes]
+$endif
+$if[$getGlobalUserVar[RVJY_2_R2]>=1]
+$addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVJY_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVJY_2_R2]\`
+Code: \`RVJY_2_R2\`;yes]
+$endif
+$if[$getGlobalUserVar[RVWD_2_R2]>=1]
+$addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVWD_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVWD_2_R2]\`
+Code: \`RVWD_2_R2\`;yes]
+$endif
+$if[$getGlobalUserVar[RVSG_2_R2]>=1]
+$addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVSG_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVSG_2_R2]\`
+Code: \`RVSG_2_R2\`;yes]
+$endif
+$if[$getGlobalUserVar[RVIR_2_R2]>=1]
+$addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVIR_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVIR_2_R2]\`
+Code: \`RVIR_2_R2\`;yes]
+$endif
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
