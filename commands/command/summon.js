8bf6f84e5f3f5f9005a58e2eaff29510cd89ed43
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-03 13:29:33 -0400

    Update summon.js

--- commands/command/summon.js
+++ commands/command/summon.js
@@ -6,7 +6,7 @@ module.exports ={
 
 $author[1;Summon;https://cdn.discordapp.com/attachments/946791137165836361/947615222951198800/greendrop.png]
 $addField[1;Code;\`\`\`$message\`\`\`;yes]
-$addField[1;Global Copies;\`\`\`$getGlobalUserVar[$message_OWNER]\`\`\`;yes]
+$addField[1;Global Copies;\`\`\`$getVar[$message_OWNER]\`\`\`;yes]
 $addField[1;Rarity;\`\`\`$getGlobalUserVar[$message_RARITY]\`\`\`;yes]
 $addField[1;Theme;\`\`\`$getGlobalUserVar[$message_THEME]\`\`\`;yes]
 $addField[1;Idol;\`\`\`$getGlobalUserVar[$message_IDOL]\`\`\`;yes]
@@ -27,7 +27,7 @@ $else
 
 $author[1;Summon;https://cdn.discordapp.com/attachments/946791137165836361/947615223664222288/reddrop.png]
 $addField[1;Code;\`\`\`$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA;BPJS_2_HY;BPJN_2_HY;BPRS_2_HY;BPLS_2_HY;RVIR_2_R2;RVSG_2_R2;RVWD_2_R2;RVJY_2_R2;RVYR_2_R2;TXYJ_2_CY;TXBG_2_CY;TXHK_2_CY;TXSB_2_CY;TXTH_2_CY]\`\`\`;yes]
-$addField[1;Global Copies;\`\`\`$getGlobalUserVar[$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA;BPJS_2_HY;BPJN_2_HY;BPRS_2_HY;BPLS_2_HY;RVIR_2_R2;RVSG_2_R2;RVWD_2_R2;RVJY_2_R2;RVYR_2_R2;TXYJ_2_CY;TXBG_2_CY;TXHK_2_CY;TXSB_2_CY;TXTH_2_CY]_OWNER]\`\`\`;yes]
+$addField[1;Global Copies;\`\`\`$getVar[$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA;BPJS_2_HY;BPJN_2_HY;BPRS_2_HY;BPLS_2_HY;RVIR_2_R2;RVSG_2_R2;RVWD_2_R2;RVJY_2_R2;RVYR_2_R2;TXYJ_2_CY;TXBG_2_CY;TXHK_2_CY;TXSB_2_CY;TXTH_2_CY]_OWNER]\`\`\`;yes]
 $addField[1;Rarity;\`\`\`$getGlobalUserVar[$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA]_RARITY]\`\`\`;yes]
 $addField[1;Theme;\`\`\`$getGlobalUserVar[$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA;BPJS_2_HY;BPJN_2_HY;BPRS_2_HY;BPLS_2_HY;RVIR_2_R2;RVSG_2_R2;RVWD_2_R2;RVJY_2_R2;RVYR_2_R2;TXYJ_2_CY;TXBG_2_CY;TXHK_2_CY;TXSB_2_CY;TXTH_2_CY]_THEME]\`\`\`;yes]
 $addField[1;Idol;\`\`\`$getGlobalUserVar[$randomText[ENHS_2_DA;ENJY_2_DA;ENJK_2_DA;ENSH_2_DA;ENSN_2_DA;ENJW_2_DA;ENNK_2_DA;BPJS_2_HY;BPJN_2_HY;BPRS_2_HY;BPLS_2_HY;RVIR_2_R2;RVSG_2_R2;RVWD_2_R2;RVJY_2_R2;RVYR_2_R2;TXYJ_2_CY;TXBG_2_CY;TXHK_2_CY;TXSB_2_CY;TXTH_2_CY]_IDOL]\`\`\`;yes]
