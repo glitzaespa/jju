cf4e915f01b59598c62b8b3b0d5a9391195a8fb0
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-07 18:12:06 -0400

    Update prefix.js

--- commands/command/prefix.js
+++ commands/command/prefix.js
@@ -1,6 +1,6 @@
 module.exports = [({
   name: "prefix",
-  code: `$setServerVar[prefix;$message[1]]
+  code: `$setServerVar[PREFIX;$message[1]]
 $description[1;Prefix set to \`$message[1]\`.]
 $onlyPerms[admin;Only administrators can use this command.]`
 })]
