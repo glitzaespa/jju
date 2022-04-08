d57787b28e227f31fcf2f46ab0fdcfe8bdcf3be8
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-08 16:25:50 -0400

    Add files via upload

---
+++ commands/command/inventory/inv-aespa.js
@@ -0,0 +1,33 @@
+module.exports = {
+  name: "inv aespa",
+  $if: "v4",
+  code:`
+
+$author[1;Inventory — æspa;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$title[1;\`$getGlobalUserVar[AEKR_1_FR_RARITY]\` $getGlobalUserVar[AEKR_1_FR_THEME]]
+
+$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
+
+$if[$getGlobalUserVar[AEKR_1_FR]>=1]
+$addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEKR_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEKR_1_FR]\`
+Code: \`AEKR_1_FR\`;yes]
+$endif
+$if[$getGlobalUserVar[AEWT_1_FR]>=1]
+$addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEWT_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEWT_1_FR]\`
+Code: \`AEWT_1_FR\`;yes]
+$endif
+$if[$getGlobalUserVar[AEGS_1_FR]>=1]
+$addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEGS_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEGS_1_FR]\`
+Code: \`AEGS_1_FR\`;yes]
+$endif
+$if[$getGlobalUserVar[AENN_1_FR]>=1]
+$addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AENN_1_FR_IDOL];Copies: \`$getGlobalUserVar[AENN_1_FR]\`
+Code: \`AENN_1_FR\`;yes]
+$endif
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
---
+++ commands/command/inventory/inv-nctdream.js
@@ -0,0 +1,45 @@
+module.exports = {
+  name: "inv nctdream",
+  $if: "v4",
+  code:`
+
+$author[1;Inventory — NCT Dream;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$title[1;\`$getGlobalUserVar[NCDMR_1_GM_RARITY]\` $getGlobalUserVar[NCDMR_1_GM_THEME]]
+
+$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
+
+$if[$getGlobalUserVar[NCDRJ_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDRJ_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDRJ_1_GM]\`
+Code: \`NCDRJ_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDMR_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDMR_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDMR_1_GM]\`
+Code: \`NCDMR_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDCL_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDCL_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDCL_1_GM]\`
+Code: \`NCDCL_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDJM_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDJM_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDJM_1_GM]\`
+Code: \`NCDJM_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDHC_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDHC_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDHC_1_GM]\`
+Code: \`NCDHC_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDJN_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDJN_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDJN_1_GM]\`
+Code: \`NCDJN_1_GM\`;yes]
+$endif
+$if[$getGlobalUserVar[NCDJS_1_GM]>=1]
+$addField[1;<:JJU_nctdream:961726510857875506> $getGlobalUserVar[NCDJS_1_GM_IDOL];Copies: \`$getGlobalUserVar[NCDJS_1_GM]\`
+Code: \`NCDJS_1_GM\`;yes]
+$endif
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
