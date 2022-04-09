c9c7c31592d6acdf40ed8c01c1d866b3d8b9f0c1
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-09 13:02:24 -0400

    Add files via upload

---
+++ commands/command/shop/buy-bepc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "purchase sscp",
+  code: `$author[1;Purchase;https://cdn.discordapp.com/attachments/946791137165836361/962364008051146803/JJU_shop2.png]
+
+    $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962390336460701726/JJU_clock.png]
+
+$setGlobalUserVar[$randomText[SOBE_LC;SOBE_DS]_$randomText[C;C;C;B;C;B;B;R;R;E];$sum[$getGlobalUserVar[$randomText[SOBE_LC;SOBE_DS]_$randomText[C;C;C;B;C;B;B;R;R;E]];1]]
+
+$setVar[$randomText[SOBE_LC;SOBE_DS]_$randomText[C;C;C;B;C;B;B;R;R;E]_OWNER;$sum[$getVar[$randomText[SOBE_LC;SOBE_DS]_$randomText[C;C;C;B;C;B;B;R;R;E]_OWNER];1]]
+
+$addField[1;Inventory Change;\`x1\` $getGlobalUserVar[$randomText[SOBE_LC;SOBE_DS]_$randomText[C;C;C;B;C;B;B;R;R;E]_NAME] has been added to your inventory.]
+    $addField[1;Balance Change;<:JJU_diamond:962386919969390684> \`x5,000\` have been removed from your balance.]
+    
+  $addTimestamp[1]
+  
+  $color[1;3af4cb]
+  
+  $thumbnail[1;$authorAvatar]
+
+$reply[$messageID;yes]
+
+$onlyIf[$getGlobalUserVar[DIAMOND]>=5000;{newEmbed:{author:Purchase — Error:https://cdn.discordapp.com/attachments/929023244126789682/929533733135056896/viewicon.png}
+  {description:You don't have enough Diamonds! This item costs <:diamond:939589274804846642> \`x5,000\`.}} {footer:$userTag:$authorAvatar} {timestamp} {color:2b7ced}]`
+}
---
+++ commands/command/shop/shop-pc.js
@@ -0,0 +1,20 @@
+module.exports = [{
+  name: "shop pc",
+  code: `$author[1;Shop — Packs;https://cdn.discordapp.com/attachments/946791137165836361/962364008051146803/JJU_shop2.png]
+
+  $description[1;> Check the [rates page](https://www.youtube.com/watch?v=dQw4w9WgXcQ) for more information!]
+
+    $addField[1;<:JJU_stagepack1:962385364713103450> Single Stage Card;
+    <:JJU_diamond:962386919969390684> *\`x5,000\`*
+<:JJU_search:962385676496691210> *\`sscp\`*;yes]
+
+    $addField[1;Example — How to purchase items;\`\`\`!purchase (item tag)\`\`\`]
+    $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962362551797162084/JJU_clock.png]
+  $addTimestamp[1]
+  
+  $color[1;3af4cb]
+  
+  $thumbnail[1;$authorAvatar]
+
+$reply[$messageID;yes]`
+}]
