27bfcb1a98871a374ea7505255737e22809fc3ac
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-09 13:06:30 -0400

    Update buy-sscp.js

--- commands/command/shop/buy-sscp.js
+++ commands/command/shop/buy-sscp.js
@@ -5,6 +5,7 @@ module.exports = {
     $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962390336460701726/JJU_clock.png]
 
 $setGlobalUserVar[$getGlobalUserVar[sscp];$sum[$getGlobalUserVar[$getGlobalUserVar[sscp]];1]]
+$setGlobalUserVar[DIAMOND;$sub[$getGlobalUserVar[DIAMOND];5000]]
 
 $setVar[$getGlobalUserVar[sscp]_OWNER;$sum[$getVar[$getGlobalUserVar[sscp]_OWNER];1]]
 
@@ -22,5 +23,5 @@ $addField[1;Inventory Change;\`x1\` $getGlobalUserVar[$getGlobalUserVar[sscp]_NA
 $reply[$messageID;yes]
 
 $onlyIf[$getGlobalUserVar[DIAMOND]>=5000;{newEmbed:{author:Purchase — Error:https://cdn.discordapp.com/attachments/929023244126789682/929533733135056896/viewicon.png}
-  {description:You don't have enough Diamonds! This item costs <:diamond:939589274804846642> \`x5,000\`.}} {footer:$userTag:$authorAvatar} {timestamp} {color:2b7ced}]`
+  {description:You don't have enough Diamonds! This item costs <:diamond:939589274804846642> \`x5,000\`.}} {footer:$userTag:$authorAvatar} {timestamp} {color:3af4cb}]`
 }
