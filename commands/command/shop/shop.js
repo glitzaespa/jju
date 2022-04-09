a4be13f86e7fe2cf4a979c009d9f0834312b1027
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-09 11:06:43 -0400

    Add files via upload

---
+++ commands/command/shop/shop.js
@@ -0,0 +1,20 @@
+module.exports = [({
+  name: "shop",
+  code: `$author[1;Shop;https://cdn.discordapp.com/attachments/946791137165836361/962364008051146803/JJU_shop2.png]
+
+  $description[1;Use the command along with a shop tag to continue with your purchase!]
+
+  $addField[1;New;\`nw\` Brand new items in the shop!;yes]  
+  $addField[1;Packs;\`pc\` Buy cards in bulk for a special price!;yes]  
+
+    $addField[1;Example;\`\`\`!shop bg\`\`\`]
+    $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962362551797162084/JJU_clock.png]
+
+  $addTimestamp[1]
+  
+  $color[1;3af4cb]
+  
+  $thumbnail[1;$authorAvatar]
+
+$reply[$messageID;yes]`
+})]
