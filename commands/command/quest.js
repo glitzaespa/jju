d3a635f13173c71698b8130efc66ff0996e78c3e
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-09 20:42:17 -0400

    Add files via upload

---
+++ commands/command/quest.js
@@ -0,0 +1,22 @@
+module.exports ={
+  name: "quest",
+  code:`
+
+$awaitMessages[$channelID;$authorID;1m;everything;questreply;<@$authorID>, you took too long! Command is now invalid.]
+
+$author[1;Quest — Intro;https://cdn.discordapp.com/attachments/949848002628038727/956639744912130138/JUYEON_SHADOW.png]
+$description[1;Guess the border's theme name and get a diamond reward!]
+$title[1;Complete the following quest!]
+$addField[1;Hint;\`\`\`$getGlobalUserVar[$getGlobalUserVar[QUEST_CODE]_HINT]\`\`\`]
+$footer[1;Answer has to be without caps nor spaces or special characters!]
+$image[1;$getGlobalUserVar[$getGlobalUserVar[QUEST_CODE]_IMAGE]]
+
+$wait[1s]
+
+$setGlobalUserVar[QUEST_CODE;$randomText[ENDA;TXCY;BPHY;RVR2;SOCYSM3;SOCYSM2;SOCYSM1]]
+
+$color[1;06ff00]
+
+$reply[$messageID;yes]
+`
+}
---
+++ commands/command/questreply.js
@@ -0,0 +1,23 @@
+module.exports ={
+  name: "questreply",
+  type: "awaited",
+  $if: "v4",
+  code:`
+
+$if[$message==$getGlobalUserVar[$getGlobalUserVar[QUEST_CODE]_ANSWER]]
+$setGlobalUserVar[DIAMOND;$sum[$getGlobalUserVar[DIAMOND];10]]
+$addField[1;Rewards;<:JJU_diamond:962386919969390684> x10]
+$author[1;Quest — Result;https://cdn.discordapp.com/attachments/949848002628038727/956639744912130138/JUYEON_SHADOW.png]
+$description[1;Quest has been successful!]
+$title[1;Congratulations!]
+$color[1;06ff00]
+$else
+$author[1;Quest Mission — Result;https://cdn.discordapp.com/attachments/949848002628038727/957023760945381486/JUYEON_SHADOW_WRONG.png]
+$description[1;Uh oh! You were caught and failed the quest!]
+$title[1;Better luck next time!]
+$color[1;ff0000]
+$endif
+
+$reply[$messageID;yes]
+`
+}
