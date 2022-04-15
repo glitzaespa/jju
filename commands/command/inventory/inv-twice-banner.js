99f155fd355fecd47eba33e2edca52cb20d6a628
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-14 20:46:35 -0400

    Update inv-twice-banner.js

--- commands/command/inventory/inv-twice-banner.js
+++ commands/command/inventory/inv-twice-banner.js
@@ -6,9 +6,9 @@ module.exports = {
 $author[1;Banner Inventory — TWICE;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
 
 $addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TW_BDR];Status: \`$getGlobalUserVar[TW_BDR_STATUS]\`
-Code: \`TWTY_3_SC\`;yes]
-$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TW_WIL];Status: \`$getGlobalUserVar[TW_WI_STATUS]\`
-Code: \`TWCY_3_SC\`;yes]
+Code: \`TW_BDR\`;yes]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TW_WI];Status: \`$getGlobalUserVar[TW_WI_STATUS]\`
+Code: \`TW_WI\`;yes]
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
