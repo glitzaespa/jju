fe12a63e3ecd69b5b5b4308f2ff72ef599170855
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-15 18:37:38 -0400

    Update shop-lm.js

--- commands/command/shop/shop-lm.js
+++ commands/command/shop/shop-lm.js
@@ -4,13 +4,17 @@ module.exports = [{
 
   $description[1;> Check the [rates page](https://www.youtube.com/watch?v=dQw4w9WgXcQ) for more information!]
 
-    $addField[1;<:JJU_stagepack1:962385364713103450> TWICE What Is Love? Package;
+    $addField[1;<:JJU_twicepack1:964601051586113616> TWICE What Is Love? Package;
     <:JJU_diamond:962386919969390684> *\`x500\`*
 <:JJU_search:962385676496691210> *\`twwicp\`*;yes]
 
-    $addField[1;<:JJU_stagepack1:962385364713103450> TWICE BDZ Repackage Package;
+    $addField[1;<:JJU_twicepack1:964601051586113616> TWICE BDZ Repackage Package;
     <:JJU_diamond:962386919969390684> *\`x500\`*
 <:JJU_search:962385676496691210> *\`twbdrcp\`*;yes]
+
+    $addField[1;<:JJU_aespapack1:964601050743062679> æspa Black Mamba Package;
+    <:JJU_diamond:962386919969390684> *\`x250\`*
+<:JJU_search:962385676496691210> *\`aebmcp\`*;yes]
 
     $addField[1;Example — How to purchase items;\`\`\`!purchase (item tag)\`\`\`]
     $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962362551797162084/JJU_clock.png]
