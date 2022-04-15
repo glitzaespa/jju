39c5ae438f0d67d7055ab290c751529ac71e94c5
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-14 20:20:46 -0400

    Create shop-lm.js

---
+++ commands/command/shop/shop-lm.js
@@ -0,0 +1,24 @@
+module.exports = [{
+  name: "shop lm",
+  code: `$author[1;Shop — Limited;https://cdn.discordapp.com/attachments/946791137165836361/962364008051146803/JJU_shop2.png]
+
+  $description[1;> Check the [rates page](https://www.youtube.com/watch?v=dQw4w9WgXcQ) for more information!]
+
+    $addField[1;<:JJU_stagepack1:962385364713103450> TWICE What Is Love? Package;
+    <:JJU_diamond:962386919969390684> *\`x500\`*
+<:JJU_search:962385676496691210> *\`twwicp\`*;yes]
+
+    $addField[1;<:JJU_stagepack1:962385364713103450> TWICE BDZ Repackage Package;
+    <:JJU_diamond:962386919969390684> *\`x500\`*
+<:JJU_search:962385676496691210> *\`twbdrcp\`*;yes]
+
+    $addField[1;Example — How to purchase items;\`\`\`!purchase (item tag)\`\`\`]
+    $footer[1;$userTag;https://cdn.discordapp.com/attachments/946791137165836361/962362551797162084/JJU_clock.png]
+  $addTimestamp[1]
+  
+  $color[1;3af4cb]
+  
+  $thumbnail[1;$authorAvatar]
+
+$reply[$messageID;yes]`
+}]
