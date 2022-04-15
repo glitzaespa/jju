c03e9de8b966564dbea8f2bd490e5de5c68939bf
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-14 20:23:46 -0400

    Create inv-twice-whatislove.js

---
+++ commands/command/inventory/inv-twice-whatislove.js
@@ -0,0 +1,54 @@
+module.exports = {
+  name: "inv twice - what is love",
+    aliases:["inv tw - wi","inv tw - wil","inv twice - wi"],
+  $if: "v4",
+  code:`
+
+$author[1;Inventory — TWICE;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$title[1;\`$getGlobalUserVar[TWTY_2_WI_RARITY]\` $getGlobalUserVar[TWTY_2_WI_THEME]]
+
+$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
+
+$if[$getGlobalUserVar[TWTY_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWTY_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWTY_2_WI]\`
+Code: \`TWTY_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWCY_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWCY_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWCY_2_WI]\`
+Code: \`TWCY_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWDH_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWDH_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWDH_2_WI]\`
+Code: \`TWDH_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWMN_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWMN_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWMN_2_WI]\`
+Code: \`TWMN_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWJH_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWJH_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWJH_2_WI]\`
+Code: \`TWJH_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWSN_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWSN_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWSN_2_WI]\`
+Code: \`TWSN_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWMM_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWMM_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWMM_2_WI]\`
+Code: \`TWMM_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWJY_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWJY_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWJY_2_WI]\`
+Code: \`TWJY_2_WI\`;yes]
+$endif
+$if[$getGlobalUserVar[TWNY_2_WI]>=1]
+$addField[1;<:JJU_twice:962401139133923388> $getGlobalUserVar[TWNY_2_WI_IDOL];Copies: \`$getGlobalUserVar[TWNY_2_WI]\`
+Code: \`TWNY_2_WI\`;yes]
+$endif
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
