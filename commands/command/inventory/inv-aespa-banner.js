534250c7d3257e3603d9a5608a693085444bfb07
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-15 18:55:45 -0400

    Create inv-aespa-banner.js

---
+++ commands/command/inventory/inv-aespa-banner.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "inv banner aespa",
+  $if: "v4",
+  code:`
+
+$author[1;Banner Inventory — æspa;https://cdn.discordapp.com/attachments/953097417295790121/957395333686624286/JJU_inventory.png]
+
+$addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AE_BM];Status: \`$getGlobalUserVar[AE_BM_STATUS]\`
+Code: \`AE_BM\`;yes]
+
+$color[1;3af4cb]
+$thumbnail[1;$userAvatar[$mentioned[1]]]
+$footer[1;$userTag • Page 1/1;https://cdn.discordapp.com/attachments/953097417295790121/957032974073860096/JJU_clock.png]
+$addTimestamp[1]`
+}
