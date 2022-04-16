b0a11f3081b920e34eea7935c4168b626cd672d1
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:50:38 -0400

    Update inv-blackpink-stage.js

--- commands/command/inventory/inv-blackpink-stage.js
+++ commands/command/inventory/inv-blackpink-stage.js
@@ -8,24 +8,14 @@ $author[1;Inventory — BLACKPINK;https://cdn.discordapp.com/attachments/9530974
 
 $title[1;\`$getGlobalUserVar[BPJS_3_SC_RARITY]\` $getGlobalUserVar[BPJS_3_SC_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[BPLS_3_SC]>=1]
 $addField[1;<:JJU_blackpink:957397108489597008> $getGlobalUserVar[BPLS_3_SC_IDOL];Copies: \`$getGlobalUserVar[BPLS_3_SC]\`
 Code: \`BPLS_3_SC\`;yes]
-$endif
-$if[$getGlobalUserVar[BPRS_3_SC]>=1]
 $addField[1;<:JJU_blackpink:957397108489597008> $getGlobalUserVar[BPRS_3_SC_IDOL];Copies: \`$getGlobalUserVar[BPRS_3_SC]\`
 Code: \`BPRS_3_SC\`;yes]
-$endif
-$if[$getGlobalUserVar[BPJN_3_SC]>=1]
 $addField[1;<:JJU_blackpink:957397108489597008> $getGlobalUserVar[BPJN_3_SC_IDOL];Copies: \`$getGlobalUserVar[BPJN_3_SC]\`
 Code: \`BPJN_3_SC\`;yes]
-$endif
-$if[$getGlobalUserVar[BPJS_3_SC]>=1]
 $addField[1;<:JJU_blackpink:957397108489597008> $getGlobalUserVar[BPJS_3_SC_IDOL];Copies: \`$getGlobalUserVar[BPJS_3_SC]\`
 Code: \`BPJS_3_SC\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
