26ae8ea2071a8f981205f942e4e0a30580f488f7
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:55:02 -0400

    Update inv-redvelvet-d2.js

--- commands/command/inventory/inv-redvelvet-d2.js
+++ commands/command/inventory/inv-redvelvet-d2.js
@@ -8,28 +8,16 @@ $author[1;Inventory — Red Velvet;https://cdn.discordapp.com/attachments/953097
 
 $title[1;\`$getGlobalUserVar[RVIR_2_R2_RARITY]\` $getGlobalUserVar[RVIR_2_R2_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[RVYR_2_R2]>=1]
 $addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVYR_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVYR_2_R2]\`
 Code: \`RVYR_2_R2\`;yes]
-$endif
-$if[$getGlobalUserVar[RVJY_2_R2]>=1]
 $addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVJY_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVJY_2_R2]\`
 Code: \`RVJY_2_R2\`;yes]
-$endif
-$if[$getGlobalUserVar[RVWD_2_R2]>=1]
 $addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVWD_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVWD_2_R2]\`
 Code: \`RVWD_2_R2\`;yes]
-$endif
-$if[$getGlobalUserVar[RVSG_2_R2]>=1]
 $addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVSG_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVSG_2_R2]\`
 Code: \`RVSG_2_R2\`;yes]
-$endif
-$if[$getGlobalUserVar[RVIR_2_R2]>=1]
 $addField[1;<:JJU_redvelvet:957400721597272115> $getGlobalUserVar[RVIR_2_R2_IDOL];Copies: \`$getGlobalUserVar[RVIR_2_R2]\`
 Code: \`RVIR_2_R2\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
