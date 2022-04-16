b39ed0b29c13f48d4b571879c2276d7ddae94de4
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:55:19 -0400

    Update inv-soyou-gottago.js

--- commands/command/inventory/inv-soyou-gottago.js
+++ commands/command/inventory/inv-soyou-gottago.js
@@ -8,12 +8,8 @@ $author[1;Inventory — SOYOU;https://cdn.discordapp.com/attachments/95309741729
 
 $title[1;\`$getGlobalUserVar[SOSY_1_GG_RARITY]\` $getGlobalUserVar[SOSY_1_GG_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[SOSY_1_GG]>=1]
 $addField[1;<:JJU_soyou:963558895870177360> $getGlobalUserVar[SOSY_1_GG_IDOL];Copies: \`$getGlobalUserVar[SOSY_1_GG]\`
 Code: \`SOSY_1_GG\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
