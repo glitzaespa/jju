1d35e5e28bfb6b1cd43c220235b037266009d7d2
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 13:01:57 -0400

    Update hackreply.js

--- commands/command/hackreply.js
+++ commands/command/hackreply.js
@@ -13,8 +13,6 @@ $title[1;Thanks for your help!]
 $color[1;06ff00]
 $else
 $author[1;Hack Mission — Result;https://cdn.discordapp.com/attachments/949848002628038727/957023760945381486/JUYEON_SHADOW_WRONG.png]
-$addField[1;Rewards;<:JJU_chestnut:962386674174803968> x100]
-$setGlobalUserVar[CHESTNUT;$sum[$getGlobalUserVar[CHESTNUT];100]]
 $description[1;Uh oh! You were caught and failed the mission!]
 $title[1;Thanks for your help!]
 $color[1;ff0000]
