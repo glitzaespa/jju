761d07f98e8b051e13c6a732561460ec440098ec
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:58:46 -0400

    Update inv-txt-cysm.js

--- commands/command/inventory/inv-txt-cysm.js
+++ commands/command/inventory/inv-txt-cysm.js
@@ -8,28 +8,16 @@ $author[1;Inventory — TXT;https://cdn.discordapp.com/attachments/9530974172957
 
 $title[1;\`$getGlobalUserVar[TXYJ_2_CY_RARITY]\` $getGlobalUserVar[TXYJ_2_CY_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[TXYJ_2_CY]>=1]
 $addField[1;<:JJU_txt:959555876937695233> $getGlobalUserVar[TXYJ_2_CY_IDOL];Copies: \`$getGlobalUserVar[TXYJ_2_CY]\`
 Code: \`TXYJ_2_CY\`;yes]
-$endif
-$if[$getGlobalUserVar[TXSB_2_CY]>=1]
 $addField[1;<:JJU_txt:959555876937695233> $getGlobalUserVar[TXSB_2_CY_IDOL];Copies: \`$getGlobalUserVar[TXSB_2_CY]\`
 Code: \`TXSB_2_CY\`;yes]
-$endif
-$if[$getGlobalUserVar[TXBG_2_CY]>=1]
 $addField[1;<:JJU_txt:959555876937695233> $getGlobalUserVar[TXBG_2_CY_IDOL];Copies: \`$getGlobalUserVar[TXBG_2_CY]\`
 Code: \`TXBG_2_CY\`;yes]
-$endif
-$if[$getGlobalUserVar[TXTH_2_CY]>=1]
 $addField[1;<:JJU_txt:959555876937695233> $getGlobalUserVar[TXTH_2_CY_IDOL];Copies: \`$getGlobalUserVar[TXTH_2_CY]\`
 Code: \`TXTH_2_CY\`;yes]
-$endif
-$if[$getGlobalUserVar[TXHK_2_CY]>=1]
 $addField[1;<:JJU_txt:959555876937695233> $getGlobalUserVar[TXHK_2_CY_IDOL];Copies: \`$getGlobalUserVar[TXHK_2_CY]\`
 Code: \`TXHK_2_CY\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
