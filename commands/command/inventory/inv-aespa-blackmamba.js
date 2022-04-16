a84114734b6beddc86c7155bdf73eff045ab5b84
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:47:37 -0400

    Update inv-aespa-blackmamba.js

--- commands/command/inventory/inv-aespa-blackmamba.js
+++ commands/command/inventory/inv-aespa-blackmamba.js
@@ -8,24 +8,14 @@ $author[1;Inventory — æspa;https://cdn.discordapp.com/attachments/95309741729
 
 $title[1;\`$getGlobalUserVar[AEKR_2_BM_RARITY]\` $getGlobalUserVar[AEKR_2_BM_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[AEKR_2_BM]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEKR_2_BM_IDOL];Copies: \`$getGlobalUserVar[AEKR_2_BM]\`
 Code: \`AEKR_2_BM\`;yes]
-$endif
-$if[$getGlobalUserVar[AEWT_2_BM]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEWT_2_BM_IDOL];Copies: \`$getGlobalUserVar[AEWT_2_BM]\`
 Code: \`AEWT_2_BM\`;yes]
-$endif
-$if[$getGlobalUserVar[AEGS_2_BM]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEGS_2_BM_IDOL];Copies: \`$getGlobalUserVar[AEGS_2_BM]\`
 Code: \`AEGS_2_BM\`;yes]
-$endif
-$if[$getGlobalUserVar[AENN_2_BM]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AENN_2_BM_IDOL];Copies: \`$getGlobalUserVar[AENN_2_BM]\`
 Code: \`AENN_2_BM\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
