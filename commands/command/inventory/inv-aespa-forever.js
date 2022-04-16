eebe45e872bf77fa04efc21865503fdd4653dcd5
glitzaespa <101361608+glitzaespa@users.noreply.github.com>
2022-04-16 12:48:12 -0400

    Update inv-aespa-forever.js

--- commands/command/inventory/inv-aespa-forever.js
+++ commands/command/inventory/inv-aespa-forever.js
@@ -8,24 +8,14 @@ $author[1;Inventory — æspa;https://cdn.discordapp.com/attachments/95309741729
 
 $title[1;\`$getGlobalUserVar[AEKR_1_FR_RARITY]\` $getGlobalUserVar[AEKR_1_FR_THEME]]
 
-$description[1;To avoid flooding issues, cards you don't own won't appear in your inventory.]
-
-$if[$getGlobalUserVar[AEKR_1_FR]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEKR_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEKR_1_FR]\`
 Code: \`AEKR_1_FR\`;yes]
-$endif
-$if[$getGlobalUserVar[AEWT_1_FR]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEWT_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEWT_1_FR]\`
 Code: \`AEWT_1_FR\`;yes]
-$endif
-$if[$getGlobalUserVar[AEGS_1_FR]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AEGS_1_FR_IDOL];Copies: \`$getGlobalUserVar[AEGS_1_FR]\`
 Code: \`AEGS_1_FR\`;yes]
-$endif
-$if[$getGlobalUserVar[AENN_1_FR]>=1]
 $addField[1;<:JJU_aespa:962084630742327397> $getGlobalUserVar[AENN_1_FR_IDOL];Copies: \`$getGlobalUserVar[AENN_1_FR]\`
 Code: \`AENN_1_FR\`;yes]
-$endif
 
 $color[1;3af4cb]
 $thumbnail[1;$userAvatar[$mentioned[1]]]
